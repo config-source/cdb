3ddd84d53a8ac888199fda9c03f73e067494726d
Mathew Robinson <mathew@chasinglogic.io>
2024-02-23 14:05:05 +0000

    chore: add database seed script

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,49 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+production = Cdb.Repo.insert!(%Cdb.Environments.Environment{name: "production"})
+
+staging =
+  Cdb.Repo.insert!(%Cdb.Environments.Environment{name: "staging", parent: production})
+
+dev1 = Cdb.Repo.insert!(%Cdb.Environments.Environment{name: "dev1", parent: staging})
+dev2 = Cdb.Repo.insert!(%Cdb.Environments.Environment{name: "dev2", parent: staging})
+
+owner = Cdb.Repo.insert!(%Cdb.Configuration.ConfigKey{name: "owner", value_type: :string})
+
+min_replicas =
+  Cdb.Repo.insert!(%Cdb.Configuration.ConfigKey{name: "min_replicas", value_type: :integer})
+
+max_replicas =
+  Cdb.Repo.insert!(%Cdb.Configuration.ConfigKey{name: "max_replicas", value_type: :integer})
+
+Cdb.Repo.insert!(%Cdb.Configuration.ConfigValue{
+  str_value: "SRE",
+  environment: production,
+  config_key: owner
+})
+
+Cdb.Repo.insert!(%Cdb.Configuration.ConfigValue{
+  int_value: 10,
+  environment: production,
+  config_key: min_replicas
+})
+
+Cdb.Repo.insert!(%Cdb.Configuration.ConfigValue{
+  int_value: 100,
+  environment: production,
+  config_key: max_replicas
+})
+
+Cdb.Repo.insert!(%Cdb.Configuration.ConfigValue{
+  int_value: 1,
+  environment: staging,
+  config_key: min_replicas
+})
+
+Cdb.Repo.insert!(%Cdb.Configuration.ConfigValue{
+  int_value: 10,
+  environment: staging,
+  config_key: max_replicas
+})
