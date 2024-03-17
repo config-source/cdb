315d208a5dc5ab34a082f7b1680144245fa81ee4
Mathew Robinson <mathew@chasinglogic.io>
2024-03-17 17:54:09 +0000

    fix: remove unnecessary column aliases

--- internal/postgres/queries/configValues/get_all_config_values_for_environment.sql
+++ internal/postgres/queries/configValues/get_all_config_values_for_environment.sql
@@ -1,14 +1,14 @@
 SELECT
-    cv.id AS id,
+    cv.id,
     cv.environment_id,
     cv.config_key_id,
-    ck.name AS name,
-    ck.value_type as value_type,
+    ck.name,
+    ck.value_type,
     cv.str_value,
     cv.int_value,
     cv.float_value,
     cv.bool_value,
-    cv.created_at as created_at
+    cv.created_at
 FROM config_values AS cv 
 INNER JOIN environments AS e ON config_values.environment_id = environments.id
 INNER JOIN config_keys AS ck ON config_values.config_key_id = config_keys.id
